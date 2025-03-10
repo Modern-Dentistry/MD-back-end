33d515ceaedafa2ba990aa808cc1a02278a8c672
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-03-10 07:59:42 +0400

    Update DoctorService.java

--- src/main/java/com/rustam/modern_dentistry/service/DoctorService.java
+++ src/main/java/com/rustam/modern_dentistry/service/DoctorService.java
@@ -1,13 +1,16 @@
 package com.rustam.modern_dentistry.service;
 
+import com.rustam.modern_dentistry.dao.entity.users.BaseUser;
 import com.rustam.modern_dentistry.dao.entity.users.Doctor;
 import com.rustam.modern_dentistry.dao.repository.DoctorRepository;
+import com.rustam.modern_dentistry.exception.custom.DoctorNotFoundException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +21,10 @@ public class DoctorService {
 
     public List<Doctor> readAll(){
         return doctorRepository.findAll();
+    }
+
+    public Doctor findById(UUID doctorId) {
+        return doctorRepository.findById(doctorId)
+                .orElseThrow(() -> new DoctorNotFoundException("There is no such doctor."));
     }
 }
