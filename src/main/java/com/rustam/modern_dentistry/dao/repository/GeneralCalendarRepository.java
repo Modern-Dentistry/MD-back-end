da9327daea260df28ab64bae7996154dd400f604
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-03-11 23:55:58 +0400

    Create GeneralCalendarRepository.java

---
+++ src/main/java/com/rustam/modern_dentistry/dao/repository/GeneralCalendarRepository.java
@@ -0,0 +1,8 @@
+package com.rustam.modern_dentistry.dao.repository;
+
+
+import com.rustam.modern_dentistry.dao.entity.GeneralCalendar;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GeneralCalendarRepository extends JpaRepository<GeneralCalendar,Long> {
+}
