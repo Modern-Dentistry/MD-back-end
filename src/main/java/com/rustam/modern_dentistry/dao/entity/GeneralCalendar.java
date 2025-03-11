8850189b9c9688285301189f55c75af92db3aa2a
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-03-11 23:55:46 +0400

    Create GeneralCalendar.java

---
+++ src/main/java/com/rustam/modern_dentistry/dao/entity/GeneralCalendar.java
@@ -0,0 +1,44 @@
+package com.rustam.modern_dentistry.dao.entity;
+
+import com.rustam.modern_dentistry.dao.entity.enums.status.Appointment;
+import com.rustam.modern_dentistry.dao.entity.enums.status.Room;
+import com.rustam.modern_dentistry.dao.entity.users.Doctor;
+import com.rustam.modern_dentistry.dao.entity.users.Patient;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "general_calendar")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class GeneralCalendar {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
+    Doctor doctor;
+
+    Room room;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    Patient patient;
+
+    Appointment appointment;
+
+    LocalDate date;
+
+    LocalTime time;
+
+    LocalTime period;
+}
