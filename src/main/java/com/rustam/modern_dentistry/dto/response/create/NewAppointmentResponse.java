b6c61df91b2940e51d158532cb0966cf91db0753
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-03-11 23:56:03 +0400

    Create NewAppointmentResponse.java

---
+++ src/main/java/com/rustam/modern_dentistry/dto/response/create/NewAppointmentResponse.java
@@ -0,0 +1,31 @@
+package com.rustam.modern_dentistry.dto.response.create;
+
+import com.rustam.modern_dentistry.dao.entity.enums.status.Appointment;
+import com.rustam.modern_dentistry.dao.entity.enums.status.Room;
+import com.rustam.modern_dentistry.dao.entity.users.Doctor;
+import com.rustam.modern_dentistry.dao.entity.users.Patient;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class NewAppointmentResponse {
+    Doctor doctor;
+    Room room;
+
+    Patient patient;
+
+    Appointment appointment;
+
+    LocalDate date;
+
+    LocalTime time;
+
+    LocalTime period;
+}
