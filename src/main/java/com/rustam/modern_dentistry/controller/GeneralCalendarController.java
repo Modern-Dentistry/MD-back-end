42a195dfab9f022bbfe52a511d23eba7da35a4cc
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-03-11 23:55:37 +0400

    Create GeneralCalendarController.java

---
+++ src/main/java/com/rustam/modern_dentistry/controller/GeneralCalendarController.java
@@ -0,0 +1,40 @@
+package com.rustam.modern_dentistry.controller;
+
+import com.rustam.modern_dentistry.dto.request.create.NewAppointmentRequest;
+import com.rustam.modern_dentistry.dto.response.create.NewAppointmentResponse;
+import com.rustam.modern_dentistry.dto.response.read.GeneralCalendarResponse;
+import com.rustam.modern_dentistry.dto.response.read.PatientReadResponse;
+import com.rustam.modern_dentistry.service.GeneralCalendarService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/api/v1/general-calendar")
+@RequiredArgsConstructor
+public class GeneralCalendarController {
+
+    private final GeneralCalendarService generalCalendarService;
+
+    @GetMapping(path = "/read-doctors")
+    public ResponseEntity<List<GeneralCalendarResponse>> readDoctors() {
+        return new ResponseEntity<>(generalCalendarService.readDoctors(), HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/new-appointment")
+    public ResponseEntity<NewAppointmentResponse> newAppointment(@Valid @RequestBody NewAppointmentRequest newAppointmentRequest){
+        return new ResponseEntity<>(generalCalendarService.newAppointment(newAppointmentRequest),HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/selecting-doctor-viewing-patient/{doctorId}")
+    public ResponseEntity<List<PatientReadResponse>> selectingDoctorViewingPatient(@PathVariable UUID doctorId){
+        return new ResponseEntity<>(generalCalendarService.selectingDoctorViewingPatient(doctorId),HttpStatus.OK);
+    }
+
+
+}
