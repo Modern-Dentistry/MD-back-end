9d9ad3933ded0cadf8471f9e8f855e778e67f8c3
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-03-12 21:26:55 +0400

    Update PatientRepository.java

--- src/main/java/com/rustam/modern_dentistry/dao/repository/PatientRepository.java
+++ src/main/java/com/rustam/modern_dentistry/dao/repository/PatientRepository.java
@@ -3,7 +3,14 @@ package com.rustam.modern_dentistry.dao.repository;
 import com.rustam.modern_dentistry.dao.entity.users.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.UUID;
 
 
 public interface PatientRepository extends JpaRepository<Patient, Long> , JpaSpecificationExecutor<Patient> {
+    @Query("SELECT p FROM Patient p WHERE p.doctor.id = :doctorId")
+    List<Patient> findAllByDoctor_Id(UUID doctorId);
+
 }
