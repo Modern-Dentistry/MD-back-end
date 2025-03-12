90100c886e4c277d0f4efe179fa09ea433caed59
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-03-12 23:08:33 +0400

    Update GeneralCalendarRepository.java

--- src/main/java/com/rustam/modern_dentistry/dao/repository/GeneralCalendarRepository.java
+++ src/main/java/com/rustam/modern_dentistry/dao/repository/GeneralCalendarRepository.java
@@ -2,7 +2,15 @@ package com.rustam.modern_dentistry.dao.repository;
 
 
 import com.rustam.modern_dentistry.dao.entity.GeneralCalendar;
+import com.rustam.modern_dentistry.dao.entity.users.Doctor;
+import com.rustam.modern_dentistry.dao.entity.users.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface GeneralCalendarRepository extends JpaRepository<GeneralCalendar,Long> {
+    @Query("SELECT g FROM GeneralCalendar g WHERE g.doctor.id = :doctorId")
+    List<GeneralCalendar> findAllByDoctor_Id(UUID doctorId);
 }
