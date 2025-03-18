00b97dd1045ef92e2ab55bd5685d92fddc78df7b
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-03-18 17:51:32 +0400

    Create UpdateTeethRequest.java

---
+++ src/main/java/com/rustam/modern_dentistry/dto/request/update/UpdateTeethRequest.java
@@ -0,0 +1,27 @@
+package com.rustam.modern_dentistry.dto.request.update;
+
+import com.rustam.modern_dentistry.dao.entity.enums.status.ToothLocation;
+import com.rustam.modern_dentistry.dao.entity.enums.status.ToothType;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UpdateTeethRequest {
+
+    @NotNull
+    Long id;
+
+    @NotNull
+    Long toothNo;
+
+    @NotNull
+    ToothType toothType;
+
+    @NotNull
+    ToothLocation toothLocation;
+}
