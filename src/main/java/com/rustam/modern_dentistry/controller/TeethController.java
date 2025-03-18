683f7aab63f38a1dab6c2489414d8f6307c4e466
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-03-18 17:51:17 +0400

    Create TeethController.java

---
+++ src/main/java/com/rustam/modern_dentistry/controller/TeethController.java
@@ -0,0 +1,51 @@
+package com.rustam.modern_dentistry.controller;
+
+import com.rustam.modern_dentistry.dto.request.create.CreateTeethRequest;
+import com.rustam.modern_dentistry.dto.request.read.TeethRequest;
+import com.rustam.modern_dentistry.dto.request.update.UpdateTeethRequest;
+import com.rustam.modern_dentistry.dto.response.read.TeethResponse;
+import com.rustam.modern_dentistry.dto.response.update.TeethUpdateResponse;
+import com.rustam.modern_dentistry.service.TeethService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/teeth")
+@RequiredArgsConstructor
+public class TeethController {
+
+    private final TeethService teethService;
+
+    @PostMapping(path = "/create")
+    public ResponseEntity<Void> create(@Valid @RequestBody CreateTeethRequest createTeethRequest){
+        teethService.create(createTeethRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(path = "/read")
+    public ResponseEntity<List<TeethResponse>> read(){
+        return new ResponseEntity<>(teethService.read(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/search")
+    public ResponseEntity<List<TeethResponse>> search(@RequestBody TeethRequest teethRequest){
+        return new ResponseEntity<>(teethService.search(teethRequest),HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/update")
+    public ResponseEntity<TeethUpdateResponse> update(@Valid @RequestBody UpdateTeethRequest updateTeethRequest){
+        return new ResponseEntity<>(teethService.update(updateTeethRequest),HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        teethService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
