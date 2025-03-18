599c79226d25ed39a7355dbcb001cd0fec769b90
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-03-18 17:51:42 +0400

    Create TeethMapper.java

---
+++ src/main/java/com/rustam/modern_dentistry/mapper/TeethMapper.java
@@ -0,0 +1,25 @@
+package com.rustam.modern_dentistry.mapper;
+
+import com.rustam.modern_dentistry.dao.entity.Teeth;
+import com.rustam.modern_dentistry.dto.response.read.TeethResponse;
+import com.rustam.modern_dentistry.dto.response.update.TeethUpdateResponse;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
+@Mapper(componentModel = SPRING,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR
+)
+public interface TeethMapper {
+    TeethMapper INSTANCE = Mappers.getMapper(TeethMapper.class);
+    @Mapping(target = "examinations", source = "examinations")
+    TeethResponse toTeethResponse(Teeth teeth);
+
+    TeethUpdateResponse toUpdateResponse(Teeth teeth);
+}
