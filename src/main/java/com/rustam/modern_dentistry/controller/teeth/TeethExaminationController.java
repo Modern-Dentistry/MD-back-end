31377a1f0d12f12695d0ef6be4c998d4a0c90811
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-03-20 14:12:30 +0400

    Create TeethExaminationController.java

---
+++ src/main/java/com/rustam/modern_dentistry/controller/teeth/TeethExaminationController.java
@@ -0,0 +1,36 @@
+package com.rustam.modern_dentistry.controller.teeth;
+
+import com.rustam.modern_dentistry.dto.request.create.TeethExaminationRequest;
+import com.rustam.modern_dentistry.dto.request.read.TeethExaminationSearchRequest;
+import com.rustam.modern_dentistry.dto.response.create.TeethExaminationResponse;
+import com.rustam.modern_dentistry.service.TeethExaminationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/teeth-examination")
+@RequiredArgsConstructor
+public class TeethExaminationController {
+
+    private final TeethExaminationService teethExaminationService;
+
+    @PostMapping(path = "/create")
+    public ResponseEntity<TeethExaminationResponse> create(@Valid @RequestBody TeethExaminationRequest teethExaminationRequest){
+        return new ResponseEntity<>(teethExaminationService.create(teethExaminationRequest), HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/read")
+    public ResponseEntity<List<TeethExaminationResponse>> read(){
+        return new ResponseEntity<>(teethExaminationService.read(),HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/search")
+    public ResponseEntity<List<TeethExaminationResponse>> search(@RequestBody TeethExaminationSearchRequest teethExaminationSearchRequest){
+        return new ResponseEntity<>(teethExaminationService.search(teethExaminationSearchRequest),HttpStatus.OK);
+    }
+}
