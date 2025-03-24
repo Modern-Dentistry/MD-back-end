ca68b83b70ed09bffbd0069d68b3be3acde08ff3
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-03-24 06:57:55 +0400

    Update TeethOperationRepository.java

--- src/main/java/com/rustam/modern_dentistry/dao/repository/TeethOperationRepository.java
+++ src/main/java/com/rustam/modern_dentistry/dao/repository/TeethOperationRepository.java
@@ -1,17 +1,24 @@
 package com.rustam.modern_dentistry.dao.repository;
 
 
+import com.rustam.modern_dentistry.dao.entity.teeth.TeethExamination;
 import com.rustam.modern_dentistry.dao.entity.teeth.TeethOperation;
 import com.rustam.modern_dentistry.dto.response.read.TeethOperationResponse;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface TeethOperationRepository extends JpaRepository<TeethOperation,Long> {
+public interface TeethOperationRepository extends JpaRepository<TeethOperation,Long>, JpaSpecificationExecutor<TeethOperation> {
 
     @Query("SELECT new com.rustam.modern_dentistry.dto.response.read.TeethOperationResponse(t.id, i.operationName) " +
             "FROM TeethOperation t " +
             "JOIN t.opTypeItem i")
     List<TeethOperationResponse> findAllTeethOperations();
+
+    @EntityGraph(attributePaths = {"teeth"}) // Əlaqəli obyektləri əvvəlcədən yüklə
+    List<TeethOperation> findAll(Specification<TeethOperation> spec);
 }
