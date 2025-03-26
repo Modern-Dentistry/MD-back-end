63fa5a67500968d7d2c92b924dac9f32750d0e22
Elnur Hasanov <71289037+elnurhasanov-dev@users.noreply.github.com>
2025-03-26 12:13:01 +0400

    Updated

--- src/main/java/com/rustam/modern_dentistry/dao/repository/OperationTypeRepository.java
+++ src/main/java/com/rustam/modern_dentistry/dao/repository/OperationTypeRepository.java
@@ -36,7 +36,6 @@ public interface OperationTypeRepository extends JpaRepository<OpType, Long>, Jp
             left join OpTypeInsurance i ON i.insuranceCompany.id = d.id AND (i.opType.id = :opTypeId OR i.opType.id IS NULL)
             """)
     List<OpInsuranceReadResponse> findByOpTypeId(@Param("opTypeId") Long opTypeId);
-    List<InsDeducReadResponse> findByOpTypeId(@Param("opTypeId") Long opTypeId);
 
     Optional<OpType> findByCategoryName(String operationCategoryName);
 }
