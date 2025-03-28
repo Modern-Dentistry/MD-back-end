6d4a2681452b776b99aab01d3a6d473b6b702d3b
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-03-28 23:49:44 +0400

    Update TeethRepository.java

--- src/main/java/com/rustam/modern_dentistry/dao/repository/TeethRepository.java
+++ src/main/java/com/rustam/modern_dentistry/dao/repository/TeethRepository.java
@@ -1,6 +1,7 @@
 package com.rustam.modern_dentistry.dao.repository;
 
 import com.rustam.modern_dentistry.dao.entity.teeth.Teeth;
+import com.rustam.modern_dentistry.dto.response.read.ExaminationResponse;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -17,6 +18,10 @@ public interface TeethRepository extends JpaRepository<Teeth,Long>, JpaSpecifica
     @EntityGraph(attributePaths = "examinations")
     List<Teeth> findAll();
 
-    @Query("SELECT t FROM Teeth t LEFT JOIN FETCH t.toothExaminations")
+    @Query("SELECT t FROM Teeth t LEFT JOIN FETCH t.toothExaminations te LEFT JOIN FETCH te.examination")
     List<Teeth> findAllWithExaminations();
+
+    @Query("SELECT new com.rustam.modern_dentistry.dto.response.read.ExaminationResponse(te.examination.id, te.examination.typeName, te.examination.status) " +
+            "FROM Teeth t JOIN t.toothExaminations te WHERE t.toothNo = :toothNo")
+    List<ExaminationResponse> findExaminationsByToothNo(Long toothNo);
 }
