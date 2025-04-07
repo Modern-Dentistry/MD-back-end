0556e6298fc16cb3bc3b2dc567ed0dfabbdf8338
Elnur Hasanov <71289037+elnurhasanov-dev@users.noreply.github.com>
2025-04-07 14:07:57 +0400

    Working on the Recipe

--- src/main/java/com/rustam/modern_dentistry/dao/repository/settings/recipes/RecipeRepository.java
+++ src/main/java/com/rustam/modern_dentistry/dao/repository/settings/recipes/RecipeRepository.java
@@ -1,7 +1,14 @@
 package com.rustam.modern_dentistry.dao.repository.settings.recipes;
 
 import com.rustam.modern_dentistry.dao.entity.settings.recipes.Recipe;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    boolean existsByNameIgnoreCase(String name);
+
+    @EntityGraph(attributePaths = {"medicines"})
+    List<Recipe> findAll();
 }
--- src/main/java/com/rustam/modern_dentistry/service/settings/recipes/RecipeService.java
+++ src/main/java/com/rustam/modern_dentistry/service/settings/recipes/RecipeService.java
@@ -1,16 +1,20 @@
 package com.rustam.modern_dentistry.service.settings.recipes;
 
+import com.rustam.modern_dentistry.dao.entity.enums.status.Status;
 import com.rustam.modern_dentistry.dao.entity.settings.recipes.Recipe;
 import com.rustam.modern_dentistry.dao.repository.settings.recipes.RecipeRepository;
 import com.rustam.modern_dentistry.dto.request.create.RecipeCreateRequest;
 import com.rustam.modern_dentistry.dto.request.update.RecipeUpdateRequest;
 import com.rustam.modern_dentistry.dto.response.RecipeReadResponse;
+import com.rustam.modern_dentistry.exception.custom.ExistsException;
 import com.rustam.modern_dentistry.exception.custom.NotFoundException;
 import com.rustam.modern_dentistry.mapper.settings.recipes.RecipeMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static com.rustam.modern_dentistry.dao.entity.enums.status.Status.ACTIVE;
 
 @Service
 @RequiredArgsConstructor
@@ -19,25 +23,41 @@ public class RecipeService {
     private final RecipeRepository recipeRepository;
 
     public void create(RecipeCreateRequest request) {
-
+        checkNameIfAlreadyExist(request.getName());
+        Recipe recipe = recipeMapper.toEntity(request);
+        recipeRepository.save(recipe);
     }
 
     public List<RecipeReadResponse> read() {
-        return null;
+        var recipes = recipeRepository.findAll();
+        return recipes.stream().map(recipeMapper::toResponse).toList();
     }
 
     public RecipeReadResponse readById(Long id) {
-        return null;
+        var recipe = getRecipeById(id);
+        return recipeMapper.toResponse(recipe);
     }
 
     public void update(Long id, RecipeUpdateRequest request) {
-
+        var recipe = getRecipeById(id);
+        recipeMapper.update(recipe, request);
+        recipeRepository.save(recipe);
     }
 
     public void updateStatus(Long id) {
+        var recipe = getRecipeById(id);
+        recipe.setStatus(recipe.getStatus() == ACTIVE ? Status.PASSIVE : ACTIVE);
+        recipeRepository.save(recipe);
     }
 
     public void delete(Long id) {
+        var recipe = getRecipeById(id);
+        recipeRepository.delete(recipe);
+    }
+
+    private void checkNameIfAlreadyExist(String name) {
+        var result = recipeRepository.existsByNameIgnoreCase(name);
+        if (result) throw new ExistsException("Bu ad artıq əlavə edilib.");
     }
 
     private Recipe getRecipeById(Long id) {
