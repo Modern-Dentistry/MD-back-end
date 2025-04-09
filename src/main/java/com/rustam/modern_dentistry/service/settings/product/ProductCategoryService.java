3714e7097dc76692a438921be9fc71e631559c80
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-04-09 14:29:21 +0400

    Update ProductCategoryService.java

--- src/main/java/com/rustam/modern_dentistry/service/settings/product/ProductCategoryService.java
+++ src/main/java/com/rustam/modern_dentistry/service/settings/product/ProductCategoryService.java
@@ -1,13 +1,17 @@
 package com.rustam.modern_dentistry.service.settings.product;
 
+import com.rustam.modern_dentistry.dao.entity.enums.status.Status;
 import com.rustam.modern_dentistry.dao.entity.settings.product.Category;
 import com.rustam.modern_dentistry.dao.repository.settings.product.CategoryRepository;
 import com.rustam.modern_dentistry.dto.request.create.ProductCategoryRequest;
+import com.rustam.modern_dentistry.dto.request.read.ProductCategorySearchRequest;
+import com.rustam.modern_dentistry.dto.request.update.ProductCategoryStatusUpdatedRequest;
 import com.rustam.modern_dentistry.dto.request.update.ProductCategoryUpdateRequest;
 import com.rustam.modern_dentistry.dto.response.create.ProductCategoryResponse;
 import com.rustam.modern_dentistry.dto.response.read.ProductCategoryReadResponse;
 import com.rustam.modern_dentistry.exception.custom.NotFoundException;
 import com.rustam.modern_dentistry.mapper.settings.product.CategoryMapper;
+import com.rustam.modern_dentistry.util.specification.settings.ProductCategorySpecification;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -26,12 +30,10 @@ public class ProductCategoryService {
     public ProductCategoryResponse create(ProductCategoryRequest productCategoryRequest) {
         Category category = Category.builder()
                 .categoryName(productCategoryRequest.getCategoryName())
+                .status(Status.ACTIVE)
                 .build();
         categoryRepository.save(category);
-        return ProductCategoryResponse.builder()
-                .id(category.getId())
-                .categoryName(productCategoryRequest.getCategoryName())
-                .build();
+        return categoryMapper.toResponse(category);
     }
 
     public Category findById(Long id){
@@ -58,14 +60,23 @@ public class ProductCategoryService {
             category.setCategoryName(productCategoryUpdateRequest.getCategoryName());
         }
         categoryRepository.save(category);
-        return ProductCategoryResponse.builder()
-                .id(productCategoryUpdateRequest.getId())
-                .categoryName(productCategoryUpdateRequest.getCategoryName())
-                .build();
+        return categoryMapper.toResponse(category);
     }
 
     public void delete(Long id) {
         Category category = findById(id);
         categoryRepository.delete(category);
+    }
+
+    public List<ProductCategoryResponse> search(ProductCategorySearchRequest productCategorySearchRequest) {
+        List<Category> categories = categoryRepository.findAll(ProductCategorySpecification.filterBy(productCategorySearchRequest));
+        return categoryMapper.toDtos(categories);
+    }
+
+    public ProductCategoryResponse statusUpdated(ProductCategoryStatusUpdatedRequest productCategoryStatusUpdatedRequest) {
+        Category category = findById(productCategoryStatusUpdatedRequest.getId());
+        category.setStatus(productCategoryStatusUpdatedRequest.getStatus());
+        categoryRepository.save(category);
+        return categoryMapper.toResponse(category);
     }
 }
