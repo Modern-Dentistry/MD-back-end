0d16a68fd170249dfd40645bf21ca7a5549de67e
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-04-09 14:29:14 +0400

    Update CategoryMapper.java

--- src/main/java/com/rustam/modern_dentistry/mapper/settings/product/CategoryMapper.java
+++ src/main/java/com/rustam/modern_dentistry/mapper/settings/product/CategoryMapper.java
@@ -1,11 +1,13 @@
 package com.rustam.modern_dentistry.mapper.settings.product;
 
 import com.rustam.modern_dentistry.dao.entity.settings.product.Category;
+import com.rustam.modern_dentistry.dto.response.create.ProductCategoryResponse;
 import com.rustam.modern_dentistry.dto.response.read.ProductCategoryReadResponse;
-import com.rustam.modern_dentistry.dto.response.read.ProductReadResponse;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
@@ -16,4 +18,8 @@ import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 )
 public interface CategoryMapper {
     ProductCategoryReadResponse toDto(Category category);
+
+    List<ProductCategoryResponse> toDtos(List<Category> categories);
+
+    ProductCategoryResponse toResponse(Category category);
 }
