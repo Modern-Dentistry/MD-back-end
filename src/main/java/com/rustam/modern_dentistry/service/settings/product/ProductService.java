cecc8ea20c6e0a5567c15baec86f10056a9198fe
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-04-09 14:29:24 +0400

    Update ProductService.java

--- src/main/java/com/rustam/modern_dentistry/service/settings/product/ProductService.java
+++ src/main/java/com/rustam/modern_dentistry/service/settings/product/ProductService.java
@@ -1,14 +1,20 @@
 package com.rustam.modern_dentistry.service.settings.product;
 
+import com.rustam.modern_dentistry.dao.entity.enums.status.Status;
 import com.rustam.modern_dentistry.dao.entity.settings.product.Category;
 import com.rustam.modern_dentistry.dao.entity.settings.product.Product;
 import com.rustam.modern_dentistry.dao.repository.settings.product.ProductRepository;
 import com.rustam.modern_dentistry.dto.request.create.ProductRequest;
+import com.rustam.modern_dentistry.dto.request.read.ProductSearchRequest;
 import com.rustam.modern_dentistry.dto.request.update.ProductUpdateRequest;
+import com.rustam.modern_dentistry.dto.request.update.ProductUpdatedStatusRequest;
 import com.rustam.modern_dentistry.dto.response.create.ProductResponse;
+import com.rustam.modern_dentistry.dto.response.read.ProductReadResponse;
 import com.rustam.modern_dentistry.exception.custom.ExistsException;
 import com.rustam.modern_dentistry.exception.custom.NotFoundException;
 import com.rustam.modern_dentistry.mapper.ModelMapperConfig;
+import com.rustam.modern_dentistry.mapper.settings.product.ProductMapper;
+import com.rustam.modern_dentistry.util.specification.settings.ProductSpecification;
 import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +33,7 @@ public class ProductService {
     ProductRepository productRepository;
     ProductCategoryService productCategoryService;
     ModelMapper modelMapper;
+    ProductMapper productMapper;
 
     public ProductResponse create(ProductRequest productRequest) {
         Category category = productCategoryService.findById(productRequest.getCategoryId());
@@ -34,13 +41,13 @@ public class ProductService {
         if (existsByProductName){
             throw new ExistsException("This product is usually already available.");
         }
-        BigDecimal sum = productRequest.getPrice().multiply(BigDecimal.valueOf(productRequest.getQuantity()));
+        //BigDecimal sum = productRequest.getPrice().multiply(BigDecimal.valueOf(productRequest.getQuantity()));
         Product product = Product.builder()
                 .category(category)
+                .productNo(productRequest.getProductNo())
+                .productTitle(productRequest.getProductTitle())
                 .productName(productRequest.getProductName())
-                .quantity(productRequest.getQuantity())
-                .price(productRequest.getPrice())
-                .sumPrice(sum)
+                .status(Status.ACTIVE)
                 .build();
         productRepository.save(product);
         return modelMapper.map(product, ProductResponse.class);
@@ -53,34 +60,35 @@ public class ProductService {
     @Transactional
     public ProductResponse update(ProductUpdateRequest productUpdateRequest) {
         Product product = findById(productUpdateRequest.getId());
-        long quantitySum = product.getQuantity();
-        if (productUpdateRequest.getCategoryId() != null){
-            Category category = productCategoryService.findById(productUpdateRequest.getCategoryId());
-            product.setCategory(category);
-        }
+        //long quantitySum = product.getQuantity();
         if (productUpdateRequest.getProductName() != null && !productUpdateRequest.getProductName().isBlank()){
             product.setProductName(productUpdateRequest.getProductName());
         }
-        if (productUpdateRequest.getQuantity() != null && productUpdateRequest.getQuantity() != 0 ){
-            quantitySum += productUpdateRequest.getQuantity();
-            product.setQuantity(quantitySum);
-            BigDecimal resultValue = product.getPrice().multiply(BigDecimal.valueOf(quantitySum));
-            product.setSumPrice(resultValue);
+        if (productUpdateRequest.getProductNo() != null && productUpdateRequest.getProductNo() != 0){
+            product.setProductNo(productUpdateRequest.getProductNo());
         }
-        if (productUpdateRequest.getPrice() != null && !productUpdateRequest.getPrice().equals(BigDecimal.ZERO)){
-            BigDecimal sum = productUpdateRequest.getPrice().multiply(BigDecimal.valueOf(quantitySum));
-            product.setPrice(productUpdateRequest.getPrice());
-            product.setSumPrice(sum);
+        if (productUpdateRequest.getProductTitle()!= null && !productUpdateRequest.getProductTitle().isBlank()){
+            product.setProductTitle(product.getProductTitle());
         }
+//        if (productUpdateRequest.getQuantity() != null && productUpdateRequest.getQuantity() != 0 ){
+//            quantitySum += productUpdateRequest.getQuantity();
+//            product.setQuantity(quantitySum);
+//            BigDecimal resultValue = product.getPrice().multiply(BigDecimal.valueOf(quantitySum));
+//            product.setSumPrice(resultValue);
+//        }
+//        if (productUpdateRequest.getPrice() != null && !productUpdateRequest.getPrice().equals(BigDecimal.ZERO)){
+//            BigDecimal sum = productUpdateRequest.getPrice().multiply(BigDecimal.valueOf(quantitySum));
+//            product.setPrice(productUpdateRequest.getPrice());
+//            product.setSumPrice(sum);
+//        }
         productRepository.save(product);
         return ProductResponse.builder()
-                .id(productUpdateRequest.getId())
+                .id(product.getId())
                 .categoryId(product.getCategory().getId())
                 .categoryName(product.getCategory().getCategoryName())
                 .productName(product.getProductName())
-                .quantity(product.getQuantity())
-                .price(product.getPrice())
-                .sumPrice(product.getSumPrice())
+                .productNo(product.getProductNo())
+                .productTitle(product.getProductTitle())
                 .build();
     }
 
@@ -98,5 +106,22 @@ public class ProductService {
         Product product = productRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("No such product found."));
         productRepository.delete(product);
+    }
+
+    public Product findByIdAndCategoryId(Long productId,Long categoryId) {
+        return productRepository.findByIdAndCategoryId(productId,categoryId)
+                .orElseThrow(() -> new NotFoundException("No such product found."));
+    }
+
+    public ProductReadResponse statusUpdated(ProductUpdatedStatusRequest productUpdatedStatusRequest) {
+        Product product = findById(productUpdatedStatusRequest.getProductId());
+        product.setStatus(productUpdatedStatusRequest.getStatus());
+        productRepository.save(product);
+        return productMapper.toDto(product);
+    }
+
+    public List<ProductReadResponse> search(ProductSearchRequest productSearchRequest) {
+        List<Product> products = productRepository.findAll(ProductSpecification.filterBy(productSearchRequest));
+        return productMapper.toDtos(products);
     }
 }
