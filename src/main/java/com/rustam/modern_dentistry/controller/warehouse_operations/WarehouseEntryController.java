75eb16acf7d8fe2eed2f114d3a870e4d1f4564e8
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-04-10 07:10:07 +0400

    Update WarehouseEntryController.java

--- src/main/java/com/rustam/modern_dentistry/controller/warehouse_operations/WarehouseEntryController.java
+++ src/main/java/com/rustam/modern_dentistry/controller/warehouse_operations/WarehouseEntryController.java
@@ -2,8 +2,10 @@ package com.rustam.modern_dentistry.controller.warehouse_operations;
 
 
 import com.rustam.modern_dentistry.dto.request.create.WarehouseEntryCreateRequest;
+import com.rustam.modern_dentistry.dto.request.read.WarehouseEntrySearchRequest;
 import com.rustam.modern_dentistry.dto.response.create.ProductCategoryResponse;
 import com.rustam.modern_dentistry.dto.response.create.WarehouseEntryCreateResponse;
+import com.rustam.modern_dentistry.dto.response.read.WarehouseEntryReadResponse;
 import com.rustam.modern_dentistry.service.warehouse_entry.WarehouseEntryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,6 +24,16 @@ public class WarehouseEntryController {
     @PostMapping(path = "/create")
     public ResponseEntity<WarehouseEntryCreateResponse> create(@RequestBody WarehouseEntryCreateRequest warehouseEntryCreateRequest){
         return new ResponseEntity<>(warehouseEntryService.create(warehouseEntryCreateRequest),HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/read")
+    public ResponseEntity<List<WarehouseEntryReadResponse>> read(){
+        return new ResponseEntity<>(warehouseEntryService.read(),HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/search")
+    public ResponseEntity<List<WarehouseEntryReadResponse>> search(@RequestBody WarehouseEntrySearchRequest warehouseEntrySearchRequest){
+        return new ResponseEntity<>(warehouseEntryService.search(warehouseEntrySearchRequest),HttpStatus.OK);
     }
 
 }
