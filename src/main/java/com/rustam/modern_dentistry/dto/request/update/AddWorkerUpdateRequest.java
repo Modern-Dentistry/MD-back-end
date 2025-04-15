6ab64fae3390be7d58eecfe8bf4cf46fb00885a5
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-04-15 23:33:57 +0400

    Update AddWorkerUpdateRequest.java

--- src/main/java/com/rustam/modern_dentistry/dto/request/update/AddWorkerUpdateRequest.java
+++ src/main/java/com/rustam/modern_dentistry/dto/request/update/AddWorkerUpdateRequest.java
@@ -45,7 +45,7 @@ public class AddWorkerUpdateRequest {
     String phone2;
     @Pattern(regexp = "\\(\\d{3}\\)-\\d{3}-\\d{2}-\\d{2}", message = "Please enter your phone number in the format (000)-000-0000.")
     String phone3;
-    @Pattern(regexp = "\\(\\d{3}\\)-\\d{3}-\\d{2}-\\d{2}", message = "Please enter your phone number in the format (000)-000-0000.")
+    @Pattern(regexp = "\\(\\d{3}\\)-\\d{3}-\\d{2}-\\d{2}", message = "Please enter your phone number in the format (000)-000-00-00.")
     String homePhone;
     @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Enter a valid email address.")
     String email;
