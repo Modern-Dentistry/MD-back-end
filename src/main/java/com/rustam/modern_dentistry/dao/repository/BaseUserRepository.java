f7a7597131a5dae1042f4c1dac4f22df591a6aaa
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-04-15 17:51:44 +0400

    Update BaseUserRepository.java

--- src/main/java/com/rustam/modern_dentistry/dao/repository/BaseUserRepository.java
+++ src/main/java/com/rustam/modern_dentistry/dao/repository/BaseUserRepository.java
@@ -3,6 +3,7 @@ package com.rustam.modern_dentistry.dao.repository;
 import com.rustam.modern_dentistry.dao.entity.users.BaseUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -10,4 +11,18 @@ import java.util.UUID;
 public interface BaseUserRepository extends JpaRepository<BaseUser, UUID>, JpaSpecificationExecutor<BaseUser> {
     Optional<BaseUser> findByUsername(String username);
     boolean existsBaseUserByEmail(String mail);
+
+    @Query(value = """
+    SELECT CASE WHEN COUNT(*) > 0 THEN TRUE ELSE FALSE END
+    FROM base_users u
+    LEFT JOIN doctors d ON d.doctor_id = u.id
+    WHERE u.username = :username
+      OR u.email = :email
+      OR u.fin_code = :finCode
+      AND (
+          u.user_type <> 'Doctor'
+          OR (:colorCode IS NULL OR d.color_code = :colorCode)
+      )
+""", nativeQuery = true)
+    boolean existsUserFully(String username, String email, String finCode, String colorCode);
 }
