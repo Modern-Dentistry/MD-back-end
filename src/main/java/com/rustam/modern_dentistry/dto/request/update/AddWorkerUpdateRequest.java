1589c304556fb5687ce4fd02b3131d2d4761af0d
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-04-15 14:39:56 +0400

    Update AddWorkerUpdateRequest.java

--- src/main/java/com/rustam/modern_dentistry/dto/request/update/AddWorkerUpdateRequest.java
+++ src/main/java/com/rustam/modern_dentistry/dto/request/update/AddWorkerUpdateRequest.java
@@ -2,6 +2,10 @@ package com.rustam.modern_dentistry.dto.request.update;
 
 import com.rustam.modern_dentistry.dao.entity.enums.Role;
 import com.rustam.modern_dentistry.dao.entity.enums.status.GenderStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -15,23 +19,37 @@ import java.util.UUID;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class AddWorkerUpdateRequest {
+    @NotNull
     UUID id;
     String username;
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!_]).{8,}$",
+            message = "Şifrə minimum 8 simvol olmalı, böyük/kiçik hərf, rəqəm və xüsusi simvol içerməlidir."
+    )
     String password;
     String name;
     String surname;
     String patronymic;
+    @Pattern(
+            regexp = "^[A-Z0-9]{7}$",
+            message = "FIN kod yalnız böyük hərflər və rəqəmlərdən ibarət 7 simvol olmalıdır."
+    )
     String finCode;
     String colorCode;
     GenderStatus genderStatus;
     LocalDate dateOfBirth;
     String degree;
+    @Pattern(regexp = "\\(\\d{3}\\)-\\d{3}-\\d{4}", message = "Please enter your phone number in the format (000)-000-0000.")
     String phone;
+    @Pattern(regexp = "\\(\\d{3}\\)-\\d{3}-\\d{4}", message = "Please enter your phone number in the format (000)-000-0000.")
     String phone2;
+    @Pattern(regexp = "\\(\\d{3}\\)-\\d{3}-\\d{4}", message = "Please enter your phone number in the format (000)-000-0000.")
+    String phone3;
+    @Pattern(regexp = "\\(\\d{3}\\)-\\d{3}-\\d{4}", message = "Please enter your phone number in the format (000)-000-0000.")
     String homePhone;
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Enter a valid email address.")
     String email;
     String address;
-    String workAddress;
     Integer experience;
     Set<Role> authorities;
 }
