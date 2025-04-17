719d23b8ba145ba22e96c8f73a74a862129c30d8
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-04-18 00:46:09 +0400

    Update GeneralCalendar.java

--- src/main/java/com/rustam/modern_dentistry/dao/entity/GeneralCalendar.java
+++ src/main/java/com/rustam/modern_dentistry/dao/entity/GeneralCalendar.java
@@ -2,6 +2,7 @@ package com.rustam.modern_dentistry.dao.entity;
 
 import com.rustam.modern_dentistry.dao.entity.enums.status.Appointment;
 import com.rustam.modern_dentistry.dao.entity.enums.status.Room;
+import com.rustam.modern_dentistry.dao.entity.settings.AppointmentType;
 import com.rustam.modern_dentistry.dao.entity.users.Doctor;
 import com.rustam.modern_dentistry.dao.entity.users.Patient;
 import jakarta.persistence.*;
@@ -10,6 +11,7 @@ import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Set;
 
 @Entity
 @Table(name = "general_calendar")
@@ -34,6 +36,14 @@ public class GeneralCalendar {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "patient_id")
     Patient patient;
+
+    @ManyToMany
+    @JoinTable(
+            name = "general_calendar_appointment_types",
+            joinColumns = @JoinColumn(name = "calendar_id"),
+            inverseJoinColumns = @JoinColumn(name = "appointment_type_id")
+    )
+    Set<AppointmentType> appointmentTypes;
 
     @Enumerated(EnumType.STRING)
     Appointment appointment;
