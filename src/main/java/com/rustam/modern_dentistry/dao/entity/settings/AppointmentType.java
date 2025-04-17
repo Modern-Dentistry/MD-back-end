3a739eef693024e1b5e63ce0513b5802bcc258e7
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-04-17 23:59:33 +0400

    Create AppointmentType.java

---
+++ src/main/java/com/rustam/modern_dentistry/dao/entity/settings/AppointmentType.java
@@ -0,0 +1,31 @@
+package com.rustam.modern_dentistry.dao.entity.settings;
+
+import com.rustam.modern_dentistry.dao.entity.enums.status.Status;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "appointment_types")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AppointmentType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "appointment_type_name")
+    String appointmentTypeName;
+
+    LocalTime time;
+
+    @Enumerated(EnumType.STRING)
+    Status status;
+
+}
