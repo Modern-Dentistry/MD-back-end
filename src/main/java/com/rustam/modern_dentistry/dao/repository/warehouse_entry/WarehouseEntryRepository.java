858981ba61a8858cb396e58c8ea9a741c0dc9aa6
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-04-21 19:48:31 +0400

    Delete WarehouseEntryRepository.java

--- src/main/java/com/rustam/modern_dentistry/dao/repository/warehouse_entry/WarehouseEntryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rustam.modern_dentistry.dao.repository.warehouse_entry;
-
-import com.rustam.modern_dentistry.dao.entity.warehouse_entry.WarehouseEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface WarehouseEntryRepository extends JpaRepository<WarehouseEntry,Long>, JpaSpecificationExecutor<WarehouseEntry> {
-}
