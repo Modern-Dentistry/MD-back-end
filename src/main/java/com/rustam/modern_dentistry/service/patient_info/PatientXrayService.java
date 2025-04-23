98f8d83b9276a38bb6ebe085f6b3d71b17c8f388
Elnur Hasanov <71289037+elnurhasanov-dev@users.noreply.github.com>
2025-04-23 10:56:02 +0400

    Working on Xray

--- src/main/java/com/rustam/modern_dentistry/service/patient_info/PatientXrayService.java
+++ src/main/java/com/rustam/modern_dentistry/service/patient_info/PatientXrayService.java
@@ -1,11 +1,15 @@
 package com.rustam.modern_dentistry.service.patient_info;
 
 import com.rustam.modern_dentistry.dao.repository.patient_info.PatientXrayRepository;
+import com.rustam.modern_dentistry.mapper.patient_info.PatientXrayMapper;
+import com.rustam.modern_dentistry.util.UtilService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PatientXrayService {
+    private final UtilService utilService;
+    private final PatientXrayMapper patientXrayMapper;
     private final PatientXrayRepository patientXrayRepository;
 }
