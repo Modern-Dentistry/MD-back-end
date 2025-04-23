39bf909939238abf2b17d292a17d60ed7cfe7d8f
Elnur Hasanov <71289037+elnurhasanov-dev@users.noreply.github.com>
2025-04-23 11:20:53 +0400

    Working on Patient Xray

--- src/main/java/com/rustam/modern_dentistry/service/patient_info/PatientXrayService.java
+++ src/main/java/com/rustam/modern_dentistry/service/patient_info/PatientXrayService.java
@@ -5,6 +5,7 @@ import com.rustam.modern_dentistry.dto.request.create.PatXrayCreateReq;
 import com.rustam.modern_dentistry.dto.request.update.PatXrayUpdateReq;
 import com.rustam.modern_dentistry.dto.response.read.PatXrayReadRes;
 import com.rustam.modern_dentistry.mapper.patient_info.PatientXrayMapper;
+import com.rustam.modern_dentistry.service.FileService;
 import com.rustam.modern_dentistry.util.UtilService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,14 +14,24 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+import static com.rustam.modern_dentistry.util.constants.Directory.pathPatPhoto;
+
 @Service
 @RequiredArgsConstructor
 public class PatientXrayService {
     private final UtilService utilService;
+    private final FileService fileService;
     private final PatientXrayMapper patientXrayMapper;
     private final PatientXrayRepository patientXrayRepository;
 
     public void create(@Valid PatXrayCreateReq request, MultipartFile file) {
+        var patient = utilService.findByPatientId(request.getPatientId());
+        var newFileName = fileService.getNewFileName(file, "patient_xray_");
+        var entity = patientXrayMapper.toEntity(request, newFileName);
+        entity.setPatient(patient);
+        fileService.checkFileIfExist(file);
+        fileService.writeFile(file, pathPatPhoto, newFileName);
+        patientXrayRepository.save(entity);
     }
 
     public List<PatXrayReadRes> read(Long patientId) {
