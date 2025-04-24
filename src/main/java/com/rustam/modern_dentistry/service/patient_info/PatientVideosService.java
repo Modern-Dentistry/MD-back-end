711be0c5f259381872e4e1b0ff0016a22c7d9ed7
Elnur Hasanov <71289037+elnurhasanov-dev@users.noreply.github.com>
2025-04-24 17:38:28 +0400

    Working on Patient Videos

--- src/main/java/com/rustam/modern_dentistry/service/patient_info/PatientVideosService.java
+++ src/main/java/com/rustam/modern_dentistry/service/patient_info/PatientVideosService.java
@@ -1,11 +1,35 @@
 package com.rustam.modern_dentistry.service.patient_info;
 
 import com.rustam.modern_dentistry.dao.repository.patient_info.PatientVideosRepository;
+import com.rustam.modern_dentistry.dto.request.create.PatVideosCreateReq;
+import com.rustam.modern_dentistry.dto.request.update.PatVideosUpdateReq;
+import com.rustam.modern_dentistry.dto.response.read.PatVideosReadRes;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PatientVideosService {
     private final PatientVideosRepository patientVideosRepository;
+
+    public void create(@Valid PatVideosCreateReq request, MultipartFile file) {
+    }
+
+    public List<PatVideosReadRes> read(Long patientId) {
+        return null;
+    }
+
+    public PatVideosReadRes readById(Long id) {
+        return null;
+    }
+
+    public void update(Long id, PatVideosUpdateReq request, MultipartFile file) {
+    }
+
+    public void delete(Long id) {
+    }
 }
