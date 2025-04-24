3b5dfec78c7c868643125cdc0a87951913d0f674
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-04-24 22:28:05 +0400

    Update OrderFromWarehouseService.java

--- src/main/java/com/rustam/modern_dentistry/service/warehouse_operations/OrderFromWarehouseService.java
+++ src/main/java/com/rustam/modern_dentistry/service/warehouse_operations/OrderFromWarehouseService.java
@@ -3,15 +3,26 @@ package com.rustam.modern_dentistry.service.warehouse_operations;
 import com.rustam.modern_dentistry.dao.entity.settings.product.Product;
 import com.rustam.modern_dentistry.dao.entity.warehouse_operations.OrderFromWarehouse;
 import com.rustam.modern_dentistry.dao.entity.warehouse_operations.OrderFromWarehouseProduct;
+import com.rustam.modern_dentistry.dao.entity.warehouse_operations.WarehouseEntry;
 import com.rustam.modern_dentistry.dao.entity.warehouse_operations.WarehouseEntryProduct;
 import com.rustam.modern_dentistry.dao.repository.warehouse_operations.OrderFromWarehouseRepository;
 import com.rustam.modern_dentistry.dto.request.create.OrderFromWarehouseCreateRequest;
+import com.rustam.modern_dentistry.dto.request.read.OrderFromWarehouseProductRequest;
+import com.rustam.modern_dentistry.dto.request.read.OrderFromWarehouseSearchRequest;
+import com.rustam.modern_dentistry.dto.request.update.OrderFromWarehouseProductUpdateRequest;
+import com.rustam.modern_dentistry.dto.request.update.OrderFromWarehouseUpdateRequest;
+import com.rustam.modern_dentistry.dto.request.update.WarehouseEntryProductUpdateRequest;
+import com.rustam.modern_dentistry.dto.request.update.WarehouseEntryUpdateRequest;
 import com.rustam.modern_dentistry.dto.response.create.WarehouseEntryCreateResponse;
 import com.rustam.modern_dentistry.dto.response.read.OrderFromWarehouseProductResponse;
+import com.rustam.modern_dentistry.dto.response.read.OrderFromWarehouseReadResponse;
 import com.rustam.modern_dentistry.dto.response.read.OrderFromWarehouseResponse;
 import com.rustam.modern_dentistry.dto.response.read.WarehouseEntryProductResponse;
+import com.rustam.modern_dentistry.exception.custom.NotFoundException;
+import com.rustam.modern_dentistry.mapper.warehouse_operations.OrderFromWarehouseMapper;
 import com.rustam.modern_dentistry.service.settings.product.ProductService;
 import com.rustam.modern_dentistry.util.UtilService;
+import com.rustam.modern_dentistry.util.specification.warehouse_entry.OrderFromWarehouseSpecification;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.AccessLevel;
@@ -19,7 +30,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
@@ -31,6 +44,7 @@ public class OrderFromWarehouseService {
     ProductService productService;
     UtilService utilService;
     WarehouseEntryProductService warehouseEntryProductService;
+    OrderFromWarehouseMapper orderFromWarehouseMapper;
 
     @Transactional
     public OrderFromWarehouseResponse create(OrderFromWarehouseCreateRequest request) {
@@ -67,6 +81,8 @@ public class OrderFromWarehouseService {
                 .description(orderFromWarehouse.getDescription())
                 .orderFromWarehouseProductResponses(productResponses)
                 .room(orderFromWarehouse.getRoom())
+                .personWhoPlacedOrder(orderFromWarehouse.getPersonWhoPlacedOrder())
+                .number(orderFromWarehouse.getNumber())
                 .build();
     }
 
@@ -86,5 +102,117 @@ public class OrderFromWarehouseService {
                             .build();
                 })
                 .collect(Collectors.toList());
+    }
+
+    public List<OrderFromWarehouseReadResponse> read() {
+        List<OrderFromWarehouse> entries = orderFromWarehouseRepository.findAll();
+        return orderFromWarehouseMapper.toDtos(entries);
+    }
+
+    @Transactional
+    public OrderFromWarehouseResponse info(Long id) {
+        OrderFromWarehouse orderFromWarehouse = findById(id);
+        return buildWarehouseEntryResponse(orderFromWarehouse, orderFromWarehouse.getOrderFromWarehouseProducts());
+    }
+
+    public OrderFromWarehouse findById(Long id){
+        return orderFromWarehouseRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("No such order from warehouse found."));
+    }
+
+    public List<OrderFromWarehouseReadResponse> search(OrderFromWarehouseSearchRequest orderFromWarehouseSearchRequest) {
+        List<OrderFromWarehouse> orderFromWarehouses = orderFromWarehouseRepository.findAll(OrderFromWarehouseSpecification.filterBy(orderFromWarehouseSearchRequest));
+        return orderFromWarehouseMapper.toDtos(orderFromWarehouses);
+    }
+
+    @Transactional
+    public OrderFromWarehouseResponse update(OrderFromWarehouseUpdateRequest orderFromWarehouseUpdateRequest) {
+        OrderFromWarehouse orderFromWarehouse = findById(orderFromWarehouseUpdateRequest.getId());
+        utilService.updateFieldIfPresent(orderFromWarehouseUpdateRequest.getDate(), orderFromWarehouse::setDate);
+        utilService.updateFieldIfPresent(orderFromWarehouseUpdateRequest.getTime(), orderFromWarehouse::setTime);
+        utilService.updateFieldIfPresent(orderFromWarehouseUpdateRequest.getDescription(), orderFromWarehouse::setDescription);
+        utilService.updateFieldIfPresent(orderFromWarehouseUpdateRequest.getRoom(), orderFromWarehouse::setRoom);
+
+        if (hasProducts(orderFromWarehouseUpdateRequest)){
+            List<OrderFromWarehouseProduct> entryProducts = buildWarehouseEntryUpdateProducts(orderFromWarehouseUpdateRequest, orderFromWarehouse);
+            orderFromWarehouse.getOrderFromWarehouseProducts().clear();
+            orderFromWarehouse.getOrderFromWarehouseProducts().addAll(entryProducts);
+            orderFromWarehouse.setNumber(entryProducts.size());
+        }
+
+        orderFromWarehouseRepository.save(orderFromWarehouse);
+        return buildWarehouseEntryResponse(orderFromWarehouse, orderFromWarehouse.getOrderFromWarehouseProducts());
+    }
+
+    private List<OrderFromWarehouseProduct> buildWarehouseEntryUpdateProducts(OrderFromWarehouseUpdateRequest orderFromWarehouseUpdateRequest, OrderFromWarehouse orderFromWarehouse) {
+        Map<Long, OrderFromWarehouseProduct> existingProductsMap = orderFromWarehouse.getOrderFromWarehouseProducts()
+                .stream()
+                .collect(Collectors.toMap(OrderFromWarehouseProduct::getId, Function.identity()));
+
+        List<OrderFromWarehouseProduct> finalList = new ArrayList<>();
+
+        Set<Long> updatedIds = new HashSet<>();
+
+        for (OrderFromWarehouseProductUpdateRequest dto : orderFromWarehouseUpdateRequest.getOrderFromWarehouseProductUpdateRequests()) {
+            Product product = productService.findByIdAndCategoryId(dto.getProductId(), dto.getCategoryId());
+            warehouseEntryProductService.decreaseProductQuantity(dto.getProductId(),dto.getQuantity());
+            if (dto.getOrderFromWarehouseProductId() != null && existingProductsMap.containsKey(dto.getOrderFromWarehouseProductId())) {
+
+                OrderFromWarehouseProduct existing = existingProductsMap.get(dto.getOrderFromWarehouseProductId());
+
+                utilService.updateFieldIfPresent(dto.getCategoryId(), existing::setCategoryId);
+                utilService.updateFieldIfPresent(dto.getProductId(), existing::setProductId);
+                utilService.updateFieldIfPresent(dto.getQuantity(), existing::setQuantity);
+
+                existing.setProductName(product.getProductName());
+                existing.setCategoryName(product.getCategory().getCategoryName());
+                existing.setProductTitle(product.getProductTitle());
+
+                finalList.add(existing);
+                updatedIds.add(dto.getOrderFromWarehouseProductId());
+            } else {
+
+                OrderFromWarehouseProduct newProduct = OrderFromWarehouseProduct.builder()
+                        .categoryId(dto.getCategoryId())
+                        .productId(dto.getProductId())
+                        .quantity(dto.getQuantity())
+                        .productName(product.getProductName())
+                        .categoryName(product.getCategory().getCategoryName())
+                        .productTitle(product.getProductTitle())
+                        .orderFromWarehouse(orderFromWarehouse)
+                        .build();
+                finalList.add(newProduct);
+            }
+        }
+
+        for (OrderFromWarehouseProduct old : existingProductsMap.values()) {
+            if (!updatedIds.contains(old.getId())) {
+                finalList.add(old);
+            }
+        }
+
+        return finalList;
+    }
+
+    private boolean hasProducts(OrderFromWarehouseUpdateRequest request) {
+        return request.getOrderFromWarehouseProductUpdateRequests() != null &&
+                !request.getOrderFromWarehouseProductUpdateRequests().isEmpty();
+    }
+
+    public void delete(Long id) {
+        OrderFromWarehouse orderFromWarehouse = findById(id);
+        orderFromWarehouseRepository.delete(orderFromWarehouse);
+    }
+
+
+    @Transactional
+    public void deleteOrderFromWarehouseProduct(Long id, Long productId) {
+        OrderFromWarehouse orderFromWarehouse = findById(id);
+
+        orderFromWarehouse.getOrderFromWarehouseProducts().removeIf(p -> p.getId().equals(productId));
+
+        orderFromWarehouse.setNumber(orderFromWarehouse.getOrderFromWarehouseProducts().size());
+
+        orderFromWarehouseRepository.save(orderFromWarehouse);
     }
 }
