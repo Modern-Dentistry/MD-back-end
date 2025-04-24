2a15ba8755358d7325c645bea38ce32ded3af3d3
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-04-24 22:28:07 +0400

    Create OrderFromWarehouseSpecification.java

---
+++ src/main/java/com/rustam/modern_dentistry/util/specification/warehouse_entry/OrderFromWarehouseSpecification.java
@@ -0,0 +1,27 @@
+package com.rustam.modern_dentistry.util.specification.warehouse_entry;
+
+import com.rustam.modern_dentistry.dao.entity.warehouse_operations.OrderFromWarehouse;
+import com.rustam.modern_dentistry.dao.entity.warehouse_operations.WarehouseEntry;
+import com.rustam.modern_dentistry.dto.request.read.OrderFromWarehouseSearchRequest;
+import com.rustam.modern_dentistry.dto.request.read.WarehouseEntrySearchRequest;
+import org.springframework.data.jpa.domain.Specification;
+
+public class OrderFromWarehouseSpecification {
+
+    public static Specification<OrderFromWarehouse> filterBy(OrderFromWarehouseSearchRequest request) {
+
+        return (root, query, cb) -> {
+            Specification<OrderFromWarehouse> spec = Specification.where(null);
+
+            if (request.getDate() != null) {
+                spec = spec.and((r, q, c) -> c.equal(r.get("date"), request.getDate()));
+            }
+
+            if (request.getTime() != null) {
+                spec = spec.and((r, q, c) -> c.equal(r.get("time"), request.getTime()));
+            }
+
+            return spec.toPredicate(root, query, cb);
+        };
+    }
+}
