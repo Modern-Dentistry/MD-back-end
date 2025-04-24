b7c9b6ea9434ba6209463714b2c162940dc816e0
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-04-24 22:27:59 +0400

    Create OrderFromWarehouseReadResponse.java

---
+++ src/main/java/com/rustam/modern_dentistry/dto/response/read/OrderFromWarehouseReadResponse.java
@@ -0,0 +1,29 @@
+package com.rustam.modern_dentistry.dto.response.read;
+
+import com.rustam.modern_dentistry.dao.entity.enums.status.Room;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class OrderFromWarehouseReadResponse {
+
+    LocalDate date;
+
+    LocalTime time;
+
+    Room room;
+
+    String description;
+
+    String personWhoPlacedOrder;
+
+    Integer number;
+}
