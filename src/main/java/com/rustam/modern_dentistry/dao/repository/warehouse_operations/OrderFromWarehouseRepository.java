2601420ef9e075dee9b263c8d7508bc9cf9a6ce5
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-04-26 19:19:30 +0400

    Update OrderFromWarehouseRepository.java

--- src/main/java/com/rustam/modern_dentistry/dao/repository/warehouse_operations/OrderFromWarehouseRepository.java
+++ src/main/java/com/rustam/modern_dentistry/dao/repository/warehouse_operations/OrderFromWarehouseRepository.java
@@ -3,6 +3,12 @@ package com.rustam.modern_dentistry.dao.repository.warehouse_operations;
 import com.rustam.modern_dentistry.dao.entity.warehouse_operations.OrderFromWarehouse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface OrderFromWarehouseRepository extends JpaRepository<OrderFromWarehouse,Long>, JpaSpecificationExecutor<OrderFromWarehouse> {
+
+//    @Query("SELECT o FROM OrderFromWarehouse o LEFT JOIN FETCH o.orderFromWarehouseProducts WHERE o.id = :id")
+//    Optional<OrderFromWarehouse> findByIdWithProducts(Long id);
 }
