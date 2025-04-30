de7386fdfc067534a92c8333b8d5a8da7bb34439
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-04-30 15:47:42 +0400

    Create WarehouseRemovalController.java

---
+++ src/main/java/com/rustam/modern_dentistry/controller/warehouse_operations/WarehouseRemovalController.java
@@ -0,0 +1,26 @@
+package com.rustam.modern_dentistry.controller.warehouse_operations;
+
+import com.rustam.modern_dentistry.dao.entity.warehouse_operations.WarehouseRemoval;
+import com.rustam.modern_dentistry.dto.request.create.WarehouseRemovalCreateRequest;
+import com.rustam.modern_dentistry.dto.response.create.WarehouseRemovalCreateResponse;
+import com.rustam.modern_dentistry.service.warehouse_operations.WarehouseRemovalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/warehouse-removal")
+@RequiredArgsConstructor
+public class WarehouseRemovalController {
+
+    private final WarehouseRemovalService warehouseRemovalService;
+
+    @PostMapping(path = "/create")
+    public ResponseEntity<WarehouseRemovalCreateResponse> create(@RequestBody WarehouseRemovalCreateRequest warehouseRemovalCreateRequest){
+        return new ResponseEntity<>(warehouseRemovalService.create(warehouseRemovalCreateRequest), HttpStatus.CREATED);
+    }
+}
