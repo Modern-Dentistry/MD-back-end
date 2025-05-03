2062aa30a75094134d99a51548d0ba5338ecdba0
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-05-03 23:01:57 +0400

    Update WarehouseRemovalProductRepository.java

--- src/main/java/com/rustam/modern_dentistry/dao/repository/warehouse_operations/WarehouseRemovalProductRepository.java
+++ src/main/java/com/rustam/modern_dentistry/dao/repository/warehouse_operations/WarehouseRemovalProductRepository.java
@@ -2,6 +2,7 @@ package com.rustam.modern_dentistry.dao.repository.warehouse_operations;
 
 import com.rustam.modern_dentistry.dao.entity.warehouse_operations.WarehouseRemovalProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface WarehouseRemovalProductRepository extends JpaRepository<WarehouseRemovalProduct,Long> {
+public interface WarehouseRemovalProductRepository extends JpaRepository<WarehouseRemovalProduct,Long> , JpaSpecificationExecutor<WarehouseRemovalProduct> {
 }
