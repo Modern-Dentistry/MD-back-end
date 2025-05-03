802a7bff0ed95f288f7ac266b098c7f8f0980c37
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-05-03 13:54:16 +0400

    Update OrderFromWarehouseProductService.java

--- src/main/java/com/rustam/modern_dentistry/service/warehouse_operations/OrderFromWarehouseProductService.java
+++ src/main/java/com/rustam/modern_dentistry/service/warehouse_operations/OrderFromWarehouseProductService.java
@@ -2,6 +2,7 @@ package com.rustam.modern_dentistry.service.warehouse_operations;
 
 import com.rustam.modern_dentistry.dao.entity.warehouse_operations.OrderFromWarehouseProduct;
 import com.rustam.modern_dentistry.dao.repository.warehouse_operations.OrderFromWarehouseProductRepository;
+import com.rustam.modern_dentistry.exception.custom.NotFoundException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -16,5 +17,10 @@ public class OrderFromWarehouseProductService {
 
     public void saveOrderFromWarehouseProduct(OrderFromWarehouseProduct matchedProduct) {
         orderFromWarehouseProductRepository.save(matchedProduct);
+    }
+
+    public OrderFromWarehouseProduct findById(Long orderFromWarehouseProductId) {
+        return orderFromWarehouseProductRepository.findById(orderFromWarehouseProductId)
+                .orElseThrow(() -> new NotFoundException("No such order from warehouse product found."));
     }
 }
