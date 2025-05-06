d8b62b5ed4dddb9219c20d16e603b034af6ffc08
Elnur Hasanov <71289037+elnurhasanov-dev@users.noreply.github.com>
2025-05-07 00:14:13 +0400

    Working on Technician

--- src/main/java/com/rustam/modern_dentistry/mapper/settings/TechnicianMapper.java
+++ src/main/java/com/rustam/modern_dentistry/mapper/settings/TechnicianMapper.java
@@ -2,6 +2,7 @@ package com.rustam.modern_dentistry.mapper.settings;
 
 import com.rustam.modern_dentistry.dao.entity.users.Technician;
 import com.rustam.modern_dentistry.dto.request.create.TechnicianCreateRequest;
+import com.rustam.modern_dentistry.dto.response.read.TechnicianReadResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
@@ -16,4 +17,6 @@ import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 public interface TechnicianMapper {
     @Mapping(target = "password", ignore = true)
     Technician toEntity(TechnicianCreateRequest technicianCreateRequest);
+
+    TechnicianReadResponse toResponse(Technician technician);
 }
