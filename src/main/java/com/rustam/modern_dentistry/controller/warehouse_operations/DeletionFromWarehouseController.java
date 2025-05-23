7868914182af0766a23652db7c2ac48c959a04bd
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-05-23 22:06:18 +0400

    Update DeletionFromWarehouseController.java

--- src/main/java/com/rustam/modern_dentistry/controller/warehouse_operations/DeletionFromWarehouseController.java
+++ src/main/java/com/rustam/modern_dentistry/controller/warehouse_operations/DeletionFromWarehouseController.java
@@ -1,15 +1,17 @@
 package com.rustam.modern_dentistry.controller.warehouse_operations;
 
 import com.rustam.modern_dentistry.dto.request.create.DeletionFromWarehouseCreateRequest;
+import com.rustam.modern_dentistry.dto.request.read.DeletionFromWarehouseSearchRequest;
+import com.rustam.modern_dentistry.dto.response.read.DeletionFromWarehouseProductResponse;
+import com.rustam.modern_dentistry.dto.response.read.DeletionFromWarehouseReadResponse;
 import com.rustam.modern_dentistry.dto.response.read.DeletionFromWarehouseResponse;
 import com.rustam.modern_dentistry.service.warehouse_operations.DeletionFromWarehouseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/deletion-from-warehouse")
@@ -22,4 +24,26 @@ public class DeletionFromWarehouseController {
     public ResponseEntity<DeletionFromWarehouseResponse> create(@RequestBody DeletionFromWarehouseCreateRequest deletionFromWarehouseCreateRequest){
         return new ResponseEntity<>(deletionFromWarehouseService.create(deletionFromWarehouseCreateRequest), HttpStatus.CREATED);
     }
+
+    @GetMapping(path = "/read")
+    public ResponseEntity<List<DeletionFromWarehouseResponse>> read(){
+        return new ResponseEntity<>(deletionFromWarehouseService.read(),HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/search")
+    public ResponseEntity<List<DeletionFromWarehouseResponse>> search(@RequestBody DeletionFromWarehouseSearchRequest deletionFromWarehouseSearchRequest){
+        return new ResponseEntity<>(deletionFromWarehouseService.search(deletionFromWarehouseSearchRequest),HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/info/{id}")
+    public ResponseEntity<DeletionFromWarehouseReadResponse> info(@PathVariable Long id){
+        return new ResponseEntity<>(deletionFromWarehouseService.info(id),HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/delete/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        deletionFromWarehouseService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
