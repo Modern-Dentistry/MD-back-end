8eda76349f0351b1c66648032f0b794d71ca5437
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-06-15 20:23:51 +0400

    Create Permission.java

---
+++ src/main/java/com/rustam/modern_dentistry/dao/entity/settings/permission/Permission.java
@@ -0,0 +1,42 @@
+package com.rustam.modern_dentistry.dao.entity.settings.permission;
+
+import com.rustam.modern_dentistry.dao.entity.enums.status.Status;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Entity
+@Table(name = "permission")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String permissionName;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @OneToMany(mappedBy = "permission", cascade = CascadeType.ALL)
+    private List<ModulePermissionEntity> modulePermissions;
+
+    @Override
+    public String toString() {
+        return "Permission{" +
+                "id=" + id +
+                ", permissionName='" + permissionName + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
+
+
