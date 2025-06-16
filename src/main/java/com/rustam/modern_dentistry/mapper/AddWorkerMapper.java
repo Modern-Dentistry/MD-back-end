f73fc0f7a3d4d58543f04eb0dff24a425d10e1f9
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-06-16 20:54:12 +0400

    Update AddWorkerMapper.java

--- src/main/java/com/rustam/modern_dentistry/mapper/AddWorkerMapper.java
+++ src/main/java/com/rustam/modern_dentistry/mapper/AddWorkerMapper.java
@@ -1,14 +1,18 @@
 package com.rustam.modern_dentistry.mapper;
 
+import com.rustam.modern_dentistry.dao.entity.settings.permission.Permission;
 import com.rustam.modern_dentistry.dao.entity.users.BaseUser;
 import com.rustam.modern_dentistry.dto.request.create.AddWorkerCreateRequest;
 import com.rustam.modern_dentistry.dto.response.create.AddWorkerCreateResponse;
 import com.rustam.modern_dentistry.dto.response.read.AddWorkerReadResponse;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
@@ -18,6 +22,14 @@ import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
         injectionStrategy = InjectionStrategy.CONSTRUCTOR
 )
 public interface AddWorkerMapper {
+    @Mapping(target = "permissions", source = "permissions")
     AddWorkerReadResponse toResponse(BaseUser baseUser);
+
+    default Set<String> mapPermissions(Set<Permission> permissions) {
+        if (permissions == null) return Set.of();
+        return permissions.stream()
+                .map(Permission::getPermissionName)
+                .collect(Collectors.toSet());
+    }
     List<AddWorkerReadResponse> toResponses(List<BaseUser> users);
 }
