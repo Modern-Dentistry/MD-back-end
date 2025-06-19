ee18041c7ed3f4d6a86ed101ec57715665d0bb3c
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-06-19 06:59:05 +0400

    Update PermissionController.java

--- src/main/java/com/rustam/modern_dentistry/controller/settings/PermissionController.java
+++ src/main/java/com/rustam/modern_dentistry/controller/settings/PermissionController.java
@@ -1,15 +1,15 @@
 package com.rustam.modern_dentistry.controller.settings;
 
 import com.rustam.modern_dentistry.dto.request.create.PermissionCreateRequest;
+import com.rustam.modern_dentistry.dto.response.read.InfoPermissionResponse;
 import com.rustam.modern_dentistry.dto.response.read.PermissionResponse;
 import com.rustam.modern_dentistry.service.settings.PermissionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/permission")
@@ -23,5 +23,14 @@ public class PermissionController {
         return new ResponseEntity<>(permissionService.create(permissionCreateRequest), HttpStatus.CREATED);
     }
 
+    @GetMapping(path = "/read")
+    public ResponseEntity<List<PermissionResponse>> read(){
+        return new ResponseEntity<>(permissionService.read(),HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/info/{id}")
+    public ResponseEntity<InfoPermissionResponse> info(@PathVariable Long id){
+        return new ResponseEntity<>(permissionService.info(id),HttpStatus.OK);
+    }
 
 }
