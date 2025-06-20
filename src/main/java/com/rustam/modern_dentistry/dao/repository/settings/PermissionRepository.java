5572d08cbb988fbef7554b4ea7377f8c78759e76
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-06-20 17:11:24 +0400

    Update PermissionRepository.java

--- src/main/java/com/rustam/modern_dentistry/dao/repository/settings/PermissionRepository.java
+++ src/main/java/com/rustam/modern_dentistry/dao/repository/settings/PermissionRepository.java
@@ -3,11 +3,12 @@ package com.rustam.modern_dentistry.dao.repository.settings;
 import com.rustam.modern_dentistry.dao.entity.settings.permission.Permission;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-public interface PermissionRepository extends JpaRepository<Permission,Long> {
+public interface PermissionRepository extends JpaRepository<Permission,Long>, JpaSpecificationExecutor<Permission> {
     Optional<Permission> findByPermissionName(String permissionName);
 
     @Query("""
