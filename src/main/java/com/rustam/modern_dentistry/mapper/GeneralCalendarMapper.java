06c75f14b5e9498992c1ef525c8aecd5271051f4
rustamhuseynov-dev <rustem.huseynov.2016@gmail.com>
2025-08-10 23:42:14 +0400

    Update GeneralCalendarMapper.java

--- src/main/java/com/rustam/modern_dentistry/mapper/GeneralCalendarMapper.java
+++ src/main/java/com/rustam/modern_dentistry/mapper/GeneralCalendarMapper.java
@@ -1,13 +1,16 @@
 package com.rustam.modern_dentistry.mapper;
 
 import com.rustam.modern_dentistry.dao.entity.GeneralCalendar;
+import com.rustam.modern_dentistry.dao.entity.settings.Cabinet;
 import com.rustam.modern_dentistry.dao.entity.users.Patient;
 import com.rustam.modern_dentistry.dto.response.create.NewAppointmentResponse;
 import com.rustam.modern_dentistry.dto.response.read.PatientReadResponse;
+import com.rustam.modern_dentistry.dto.response.read.ReadRooms;
 import com.rustam.modern_dentistry.dto.response.read.SelectingDoctorViewingPatientResponse;
 import com.rustam.modern_dentistry.dto.response.read.SelectingPatientToReadResponse;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
@@ -21,11 +24,17 @@ import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 )
 public interface GeneralCalendarMapper {
 
+    @Mapping(target = "cabinetName", source = "cabinet.cabinetName")
     NewAppointmentResponse toCreate(GeneralCalendar generalCalendar);
 
+    @Mapping(target = "cabinetName", source = "cabinet.cabinetName")
     SelectingDoctorViewingPatientResponse toDto(GeneralCalendar generalCalendar);
 
+    @Mapping(target = "cabinetName", source = "cabinet.cabinetName")
     List<SelectingDoctorViewingPatientResponse> toResponse(List<GeneralCalendar> allByDoctor);
 
     SelectingPatientToReadResponse toRead(GeneralCalendar calendar);
+
+    @Mapping(target = "cabinetName", source = "cabinet.cabinetName")
+    List<ReadRooms> toCabinetDto(List<Cabinet> allByCabinetName);
 }
