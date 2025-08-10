1a8b2c7936c7fd41431c83b875323a214fcd85a2
rustamhuseynov-dev <rustem.huseynov.2016@gmail.com>
2025-08-10 23:39:51 +0400

    Update CabinetRepository.java

--- src/main/java/com/rustam/modern_dentistry/dao/repository/settings/CabinetRepository.java
+++ src/main/java/com/rustam/modern_dentistry/dao/repository/settings/CabinetRepository.java
@@ -4,10 +4,12 @@ import com.rustam.modern_dentistry.dao.entity.settings.Cabinet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CabinetRepository extends JpaRepository<Cabinet,Long> , JpaSpecificationExecutor<Cabinet> {
     Optional<Cabinet> findByCabinetName(String cabinetName);
 
     boolean existsCabinetByCabinetName(String cabinetName);
+
 }
