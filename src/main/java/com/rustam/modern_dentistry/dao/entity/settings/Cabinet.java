b644234426b9eafab1c27ae116ac28c3fe52b8fc
rustamhuseynov-dev <rustem.huseynov.2016@gmail.com>
2025-08-10 23:39:20 +0400

    Update Cabinet.java

--- src/main/java/com/rustam/modern_dentistry/dao/entity/settings/Cabinet.java
+++ src/main/java/com/rustam/modern_dentistry/dao/entity/settings/Cabinet.java
@@ -1,7 +1,11 @@
 package com.rustam.modern_dentistry.dao.entity.settings;
 
 import com.rustam.modern_dentistry.dao.entity.GeneralCalendar;
+import com.rustam.modern_dentistry.dao.entity.WorkersWorkSchedule;
 import com.rustam.modern_dentistry.dao.entity.enums.status.Status;
+import com.rustam.modern_dentistry.dao.entity.warehouse_operations.OrderFromWarehouse;
+import com.rustam.modern_dentistry.dao.entity.warehouse_operations.WarehouseReceipts;
+import com.rustam.modern_dentistry.dao.entity.warehouse_operations.WarehouseRemoval;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -27,5 +31,18 @@ public class Cabinet {
 
     @OneToMany(mappedBy = "cabinet", cascade = CascadeType.ALL, orphanRemoval = true)
     List<GeneralCalendar> generalCalendars;
+
+    @OneToMany(mappedBy = "cabinet", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<OrderFromWarehouse> orderFromWarehouses;
+
+    @OneToMany(mappedBy = "cabinet", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<WarehouseReceipts> warehouseReceipts;
+
+    @OneToMany(mappedBy = "cabinet", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<WarehouseRemoval> warehouseRemovals;
+
+    @OneToMany(mappedBy = "cabinet", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<WorkersWorkSchedule> workersWorkSchedules;
+
 }
 
