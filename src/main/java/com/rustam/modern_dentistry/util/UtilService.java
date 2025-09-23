1077925a1d21b4f5fca5193c8042a43bc36a2762
rustamhuseynov-dev <rustem.huseynov.2016@gmail.com>
2025-09-23 12:14:54 +0400

    Update UtilService.java

--- src/main/java/com/rustam/modern_dentistry/util/UtilService.java
+++ src/main/java/com/rustam/modern_dentistry/util/UtilService.java
@@ -1,35 +1,24 @@
 package com.rustam.modern_dentistry.util;
 
 import com.rustam.modern_dentistry.dao.entity.users.BaseUser;
-import com.rustam.modern_dentistry.dao.entity.users.Doctor;
 import com.rustam.modern_dentistry.dao.entity.users.Patient;
-import com.rustam.modern_dentistry.dao.entity.users.Reception;
 import com.rustam.modern_dentistry.dao.entity.warehouse_operations.OrderFromWarehouse;
-import com.rustam.modern_dentistry.dao.entity.warehouse_operations.OrderFromWarehouseProduct;
 import com.rustam.modern_dentistry.dao.repository.BaseUserRepository;
-import com.rustam.modern_dentistry.dao.repository.DoctorRepository;
 import com.rustam.modern_dentistry.dao.repository.PatientRepository;
-import com.rustam.modern_dentistry.dao.repository.ReceptionRepository;
 import com.rustam.modern_dentistry.dao.repository.warehouse_operations.OrderFromWarehouseRepository;
 import com.rustam.modern_dentistry.dto.response.TokenPair;
-import com.rustam.modern_dentistry.exception.custom.DoctorNotFoundException;
 import com.rustam.modern_dentistry.exception.custom.InvalidUUIDFormatException;
 import com.rustam.modern_dentistry.exception.custom.NotFoundException;
 import com.rustam.modern_dentistry.exception.custom.UserNotFountException;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.function.Consumer;
@@ -79,6 +68,10 @@ public class UtilService {
         return null;
     }
 
+    public Optional<BaseUser> findByUsernameAndEmailAndColorCode(String username,String email,String colorCode){
+        return baseUserRepository.findByUsernameOrEmailOrFinCode(username,email,colorCode);
+    }
+
     public BaseUser findByBaseUserId(String currentUserId) {
         return baseUserRepository.findById(UUID.fromString(currentUserId))
                 .orElseThrow(() -> new UserNotFountException("No such user found."));
@@ -87,10 +80,6 @@ public class UtilService {
     public BaseUser findByBaseUserIdWithPermissions(String userId){
         return baseUserRepository.findUserWithAllPermissions(UUID.fromString(userId))
                 .orElseThrow(() -> new UserNotFountException("No such user found."));
-    }
-
-    public boolean existsByUsernameAndEmailAndFinCodeAndColorCode(String username, String email,String finCode,String colorCode) {
-        return baseUserRepository.existsUserFully(username,email,finCode,colorCode);
     }
 
     public  <T> void updateFieldIfPresent(T newValue, Consumer<T> setter) {
