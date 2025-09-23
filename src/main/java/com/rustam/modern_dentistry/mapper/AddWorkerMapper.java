ebab9d6a9c947a6656759bc7c30abfa851e033d1
rustamhuseynov-dev <rustem.huseynov.2016@gmail.com>
2025-09-23 12:13:18 +0400

    Update AddWorkerMapper.java

--- src/main/java/com/rustam/modern_dentistry/mapper/AddWorkerMapper.java
+++ src/main/java/com/rustam/modern_dentistry/mapper/AddWorkerMapper.java
@@ -3,14 +3,14 @@ package com.rustam.modern_dentistry.mapper;
 import com.rustam.modern_dentistry.dao.entity.settings.permission.Permission;
 import com.rustam.modern_dentistry.dao.entity.users.BaseUser;
 import com.rustam.modern_dentistry.dto.request.create.AddWorkerCreateRequest;
+import com.rustam.modern_dentistry.dto.request.update.AddWorkerUpdateRequest;
 import com.rustam.modern_dentistry.dto.response.create.AddWorkerCreateResponse;
 import com.rustam.modern_dentistry.dto.response.read.AddWorkerReadResponse;
 import com.rustam.modern_dentistry.dto.response.read.AddWorkerReadStatusResponse;
-import org.mapstruct.InjectionStrategy;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.ReportingPolicy;
+import com.rustam.modern_dentistry.dto.response.update.AddWorkerUpdateResponse;
+import org.mapstruct.*;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -23,6 +23,7 @@ import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
         injectionStrategy = InjectionStrategy.CONSTRUCTOR
 )
 public interface AddWorkerMapper {
+    
     @Mapping(target = "permissions", source = "permissions")
     AddWorkerReadResponse toResponse(BaseUser baseUser);
 
@@ -35,4 +36,26 @@ public interface AddWorkerMapper {
     List<AddWorkerReadResponse> toResponses(List<BaseUser> users);
 
     List<AddWorkerReadStatusResponse> toPermissionResponses(List<BaseUser> all);
+
+    @Mapping(target = "permissions", source = "permissions")
+    BaseUser dtoToEntity(@MappingTarget BaseUser baseUser, AddWorkerCreateRequest dto);
+
+    default Set<Permission> map(Set<String> permissionNames) {
+        if (permissionNames == null) {
+            return new HashSet<>();
+        }
+        return permissionNames.stream()
+                .map(name -> Permission.builder()
+                        .permissionName(name)
+                        .build())
+                .collect(Collectors.toSet());
+    }
+
+    AddWorkerCreateResponse entityToDto(BaseUser baseUser);
+
+    @Mapping(target = "permissions", source = "permissions")
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    BaseUser dtoToEntityUpdate(@MappingTarget BaseUser baseUser, AddWorkerUpdateRequest dto);
+
+    AddWorkerUpdateResponse updateDtoToResponse(BaseUser entityUpdate);
 }
