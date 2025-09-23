974d338530ed51fe433031b657e03d5c484fed79
rustamhuseynov-dev <rustem.huseynov.2016@gmail.com>
2025-09-23 12:11:31 +0400

    Update BaseUserRepository.java

--- src/main/java/com/rustam/modern_dentistry/dao/repository/BaseUserRepository.java
+++ src/main/java/com/rustam/modern_dentistry/dao/repository/BaseUserRepository.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 public interface BaseUserRepository extends JpaRepository<BaseUser, UUID>, JpaSpecificationExecutor<BaseUser> {
@@ -18,19 +19,19 @@ public interface BaseUserRepository extends JpaRepository<BaseUser, UUID>, JpaSp
 
     boolean existsBaseUserByEmail(String mail);
 
-    @Query(value = """
-    SELECT CASE WHEN COUNT(*) > 0 THEN TRUE ELSE FALSE END
-    FROM base_users u
-    LEFT JOIN doctors d ON d.doctor_id = u.id
-    LEFT JOIN authorities a ON u.id = a.base_user_id
-    LEFT JOIN permission p ON a.permission_id = p.id
-    WHERE (u.username = :username OR u.email = :email OR u.fin_code = :finCode)
-      AND (
-          p.permission_name <> 'DOCTOR'
-          OR (:colorCode IS NULL OR d.color_code = :colorCode)
-      )
-    """, nativeQuery = true)
-    boolean existsUserFully(String username, String email, String finCode, String colorCode);
+//    @Query(value = """
+//    SELECT CASE WHEN COUNT(*) > 0 THEN TRUE ELSE FALSE END
+//    FROM base_users u
+//    LEFT JOIN base_users d ON d.base_user_id = u.id
+//    LEFT JOIN authorities a ON u.id = a.base_user_id
+//    LEFT JOIN permission p ON a.permission_id = p.id
+//    WHERE (u.username = :username OR u.email = :email OR u.fin_code = :finCode)
+//      AND (
+//          p.permission_name <> 'DOCTOR'
+//          OR (:colorCode IS NULL OR d.color_code = :colorCode)
+//      )
+//    """, nativeQuery = true)
+//    boolean existsUserFully(String username, String email, String finCode, String colorCode);
 
 
 
@@ -54,10 +55,9 @@ public interface BaseUserRepository extends JpaRepository<BaseUser, UUID>, JpaSp
     boolean existsByUsernameOrEmailOrFinCode(String username, String email, String finCode);
 
     @EntityGraph(attributePaths = "permissions")
-    Optional<BaseUser> findByUsernameOrEmailOrFinCode(@NotBlank(message = "İstifadəçi Ad boş ola bilməz") @Size(min = 3, max = 20, message = "İstifadəçi Adı 3-20 simvol arasında olmalıdır") String username, @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Enter a valid email address.") String email, @Pattern(
-            regexp = "^$|^[A-Z0-9]{7}$",
-            message = "FIN kod yalnız böyük hərflər və rəqəmlərdən ibarət 7 simvol olmalıdır."
-    ) String finCode);
+    Optional<BaseUser> findByUsernameOrEmailOrFinCode(String username,String email,String finCode);
+
+    List<BaseUser> findAllByPermissionsPermissionNameIn(Set<String> visiblePermissions);
 
 //    @Query("""
 //                SELECT u FROM BaseUser u
