05b3b540482d438545350f36919be2e0cf22202f
rustamhuseynov-dev <rustem.huseynov.2016@gmail.com>
2025-09-23 12:12:06 +0400

    Update PatientRepository.java

--- src/main/java/com/rustam/modern_dentistry/dao/repository/PatientRepository.java
+++ src/main/java/com/rustam/modern_dentistry/dao/repository/PatientRepository.java
@@ -12,13 +12,11 @@ import java.util.UUID;
 
 
 public interface PatientRepository extends JpaRepository<Patient, Long>, JpaSpecificationExecutor<Patient> {
-    @Query("SELECT p FROM Patient p WHERE p.doctor.id = :doctorId")
-    List<Patient> findAllByDoctor_Id(UUID doctorId);
 
     @EntityGraph(attributePaths = {"reservations", "generalCalendars", "examinations"})
     Optional<Patient> findById(Long id);
 
-    @EntityGraph(attributePaths = {"doctor"})
+    @EntityGraph(attributePaths = {"baseUsers"})
     List<Patient> findAll();
 
     boolean existsByEmailOrFinCode(String email, String finCode);
