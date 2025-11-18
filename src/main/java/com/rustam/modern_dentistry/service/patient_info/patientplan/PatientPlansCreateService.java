a453290ea4467649cf982a620b7ab88e52ba7d49
rustamhuseynov-dev <rustem.huseynov.2016@gmail.com>
2025-11-18 17:36:41 +0400

    Create PatientPlansCreateService.java

---
+++ src/main/java/com/rustam/modern_dentistry/service/patient_info/patientplan/PatientPlansCreateService.java
@@ -0,0 +1,90 @@
+package com.rustam.modern_dentistry.service.patient_info.patientplan;
+
+import com.rustam.modern_dentistry.dao.entity.patient_info.patientplan.PatientPlan;
+import com.rustam.modern_dentistry.dao.entity.patient_info.patientplan.PatientPlanMain;
+import com.rustam.modern_dentistry.dao.entity.patient_info.patientplan.PatientPlanPartOfToothDetail;
+import com.rustam.modern_dentistry.dao.entity.settings.operations.OpTypeItem;
+import com.rustam.modern_dentistry.dao.repository.patient_info.patientplan.PatientPlansRepository;
+import com.rustam.modern_dentistry.dto.request.create.PatientPlansCreateRequest;
+import com.rustam.modern_dentistry.dto.response.create.PatientPlansResponse;
+import com.rustam.modern_dentistry.dto.response.read.CategoryOfOperationDto;
+import com.rustam.modern_dentistry.dto.response.read.PatientPlanPartOfToothDetailDto;
+import com.rustam.modern_dentistry.dto.response.read.PatientPlansDto;
+import com.rustam.modern_dentistry.exception.custom.ExistsException;
+import com.rustam.modern_dentistry.exception.custom.NotFoundException;
+import com.rustam.modern_dentistry.mapper.patient_info.patientplan.PatientPlansMainMapper;
+import com.rustam.modern_dentistry.mapper.patient_info.patientplan.PatientPlansMapper;
+import com.rustam.modern_dentistry.service.settings.operations.OperationTypeItemService;
+import com.rustam.modern_dentistry.service.settings.operations.OperationTypeService;
+import com.rustam.modern_dentistry.util.DateTimeUtil;
+import com.rustam.modern_dentistry.util.PatientPlanUtilService;
+import com.rustam.modern_dentistry.util.UtilService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class PatientPlansCreateService {
+
+    PatientPlansRepository patientPlansRepository;
+    PatientPlansUpdateMainService patientPlansUpdateMainService;
+    OperationTypeService operationTypeService;
+    UtilService utilService;
+    OperationTypeItemService operationTypeItemService;
+    PatientPlanUtilService patientPlanUtilService;
+
+    @Transactional
+    public PatientPlansResponse create(PatientPlansCreateRequest req) {
+        patientPlanUtilService.validate(
+                req.getPatientPlanMainId(),req.getToothId(),req.getCategoryId(),req.getPartOfTeethIds(),req.getOperationId()
+        );
+
+        PatientPlanMain mainPlan = patientPlansUpdateMainService
+                .findByIdAndStatusAndActionStatus(req.getPatientPlanMainId());
+
+        OpTypeItem operations = operationTypeItemService.findById(req.getOperationId());
+        if (!operations.getOpType().getId().equals(req.getCategoryId())) {
+            throw new NotFoundException("Bütün əməliyyatlar seçilmiş kateqoriyaya aid olmalıdır");
+        }
+
+        PatientPlan patientPlan = PatientPlan.builder()
+                .patientPlanMain(mainPlan)
+                .toothId(req.getToothId())
+                .opType(operationTypeService.findById(req.getCategoryId()))
+                .status("C")
+                .actionStatus("C")
+                .createdBy(utilService.getCurrentUserId())
+                .createdDate(DateTimeUtil.toEpochMilli(LocalDateTime.now()))
+                .build();
+
+        List<PatientPlanPartOfToothDetail> details = new ArrayList<>();
+
+        for (Long partOfToothId : req.getPartOfTeethIds()) {
+                PatientPlanPartOfToothDetail detail = PatientPlanPartOfToothDetail.builder()
+                        .patientPlan(patientPlan)
+                        .partOfToothId(partOfToothId)
+                        .opTypeItem(operations)
+                        .build();
+
+                details.add(detail);
+        }
+
+        patientPlan.setDetails(details);
+
+        PatientPlan savedPlan = patientPlansRepository.save(patientPlan);
+
+        return patientPlanUtilService.mapper(savedPlan);
+    }
+
+}
