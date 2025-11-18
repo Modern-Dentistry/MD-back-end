d76b1748ac100af9d77edb4a3f88a4a66847a452
rustamhuseynov-dev <rustem.huseynov.2016@gmail.com>
2025-11-18 17:35:41 +0400

    Create PatientPlansDto.java

---
+++ src/main/java/com/rustam/modern_dentistry/dto/response/read/PatientPlansDto.java
@@ -0,0 +1,20 @@
+package com.rustam.modern_dentistry.dto.response.read;
+
+
+import com.rustam.modern_dentistry.dao.entity.patient_info.patientplan.PatientPlanPartOfToothDetail;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class PatientPlansDto {
+
+    Long toothNo;
+    CategoryOfOperationDto categoryOfOperationDto;
+    List<PatientPlanPartOfToothDetailDto> partOfTeethIds;
+}
