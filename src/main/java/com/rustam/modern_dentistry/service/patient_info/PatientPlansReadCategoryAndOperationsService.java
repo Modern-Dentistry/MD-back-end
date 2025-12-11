6e0bc4a4250b086d33d6690f292079c79917fd8c
rustamhuseynov-dev <rustem.huseynov.2016@gmail.com>
2025-12-11 23:37:41 +0400

    Update PatientPlansReadCategoryAndOperationsService.java

--- src/main/java/com/rustam/modern_dentistry/service/patient_info/PatientPlansReadCategoryAndOperationsService.java
+++ src/main/java/com/rustam/modern_dentistry/service/patient_info/PatientPlansReadCategoryAndOperationsService.java
@@ -1,15 +1,8 @@
 package com.rustam.modern_dentistry.service.patient_info;
 
-import com.rustam.modern_dentistry.dao.entity.patient_info.patientplan.PatientPlan;
-import com.rustam.modern_dentistry.dto.request.create.PatientPlansCreateRequest;
-import com.rustam.modern_dentistry.dto.response.create.PatientPlansResponse;
+import com.rustam.modern_dentistry.dto.request.read.PatientPlansReadCategoryAndOperationsRequest;
 import com.rustam.modern_dentistry.dto.response.read.CategoryOfOperationDto;
-import com.rustam.modern_dentistry.dto.response.read.OpTypeItemReadResponse;
-import com.rustam.modern_dentistry.dto.response.read.PatientPlansCategoryAndOperationsResponse;
-import com.rustam.modern_dentistry.mapper.patient_info.patientplan.PatientPlansMapper;
-import com.rustam.modern_dentistry.service.settings.operations.OperationTypeItemService;
 import com.rustam.modern_dentistry.service.settings.operations.OperationTypeService;
-import com.rustam.modern_dentistry.util.UtilService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -23,15 +16,9 @@ import java.util.List;
 public class PatientPlansReadCategoryAndOperationsService {
 
     OperationTypeService operationTypeService;
-    OperationTypeItemService operationTypeItemService;
 
-    public PatientPlansCategoryAndOperationsResponse readCategoryAndOperations() {
-        List<CategoryOfOperationDto> categoryOfOperationDtos = operationTypeService.readCategoryOfOperations();
-        List<OpTypeItemReadResponse> opTypeItemReadResponses = operationTypeItemService.readAll();
-        return PatientPlansCategoryAndOperationsResponse.builder()
-                .categoryOfOperationDtos(categoryOfOperationDtos)
-                .opTypeItemReadResponses(opTypeItemReadResponses)
-                .build();
+    public List<CategoryOfOperationDto> readCategoryAndOperations(PatientPlansReadCategoryAndOperationsRequest req) {
+        return operationTypeService.getOperationsByInsurance(req.getInsuranceId());
     }
 
 }
