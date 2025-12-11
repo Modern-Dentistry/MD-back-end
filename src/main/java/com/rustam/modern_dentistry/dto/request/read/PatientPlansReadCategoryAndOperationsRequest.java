a8f9a808d178c44c8b67401650194e7ca81fb89e
rustamhuseynov-dev <rustem.huseynov.2016@gmail.com>
2025-12-11 23:36:47 +0400

    Create PatientPlansReadCategoryAndOperationsRequest.java

---
+++ src/main/java/com/rustam/modern_dentistry/dto/request/read/PatientPlansReadCategoryAndOperationsRequest.java
@@ -0,0 +1,15 @@
+package com.rustam.modern_dentistry.dto.request.read;
+
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PatientPlansReadCategoryAndOperationsRequest {
+
+    Long insuranceId;
+}
