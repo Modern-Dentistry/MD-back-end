2e87f74d378a5a3bd4fc503391d2727f591d2a12
rustamhuseynov-dev <rustem.huseynov.2016@gmail.com>
2025-12-15 17:11:14 +0400

    Create PatientPlansReadByPatientPlanMainService.java

---
+++ src/main/java/com/rustam/modern_dentistry/service/patient_info/patientplan/PatientPlansReadByPatientPlanMainService.java
@@ -0,0 +1,33 @@
+package com.rustam.modern_dentistry.service.patient_info.patientplan;
+
+import com.rustam.modern_dentistry.dao.entity.patient_info.patientplan.PatientPlan;
+import com.rustam.modern_dentistry.dao.repository.patient_info.patientplan.PatientPlansRepository;
+import com.rustam.modern_dentistry.dto.response.create.PatientPlansResponse;
+import com.rustam.modern_dentistry.dto.response.read.PatientPlansDto;
+import com.rustam.modern_dentistry.exception.custom.NotFoundException;
+import com.rustam.modern_dentistry.mapper.patient_info.patientplan.PatientPlansMainMapper;
+import com.rustam.modern_dentistry.util.PatientPlanUtilService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = true)
+public class PatientPlansReadByPatientPlanMainService {
+
+    PatientPlansRepository patientPlansRepository;
+    PatientPlanUtilService patientPlanUtilService;
+
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    public PatientPlansResponse readByPatientPlanMainId(UUID id) {
+        PatientPlan patientPlan = patientPlansRepository.findByPatientPlanMainIdAndStatusAndActionStatus(id, "A", "A")
+                .orElseThrow(() -> new NotFoundException("No such patient plan found with id: " + id));
+        return patientPlanUtilService.mapper(patientPlan);
+    }
+}
