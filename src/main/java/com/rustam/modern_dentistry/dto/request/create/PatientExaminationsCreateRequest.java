f3c4ee1fd55a4df3f83ba248ee47c781531f994a
rustamhuseynov-dev <rustem.huseynov.2016@gmail.com>
2026-01-05 13:01:03 +0400

    Update PatientExaminationsCreateRequest.java

--- src/main/java/com/rustam/modern_dentistry/dto/request/create/PatientExaminationsCreateRequest.java
+++ src/main/java/com/rustam/modern_dentistry/dto/request/create/PatientExaminationsCreateRequest.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class PatientExaminationsCreateRequest {
 
     Long patientId;
-    List<Long> toothNumber;
+    Long toothId;
+    List<Long> partOfTeethIds;
     Long examinationId;
 }
