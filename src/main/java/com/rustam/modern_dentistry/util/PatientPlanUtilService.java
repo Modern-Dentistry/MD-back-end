aef87922a24009e734809b35cf02d54bc26b7411
rustamhuseynov-dev <rustem.huseynov.2016@gmail.com>
2026-01-05 13:02:33 +0400

    Update PatientPlanUtilService.java

--- src/main/java/com/rustam/modern_dentistry/util/PatientPlanUtilService.java
+++ src/main/java/com/rustam/modern_dentistry/util/PatientPlanUtilService.java
@@ -1,13 +1,12 @@
 package com.rustam.modern_dentistry.util;
 
 import com.rustam.modern_dentistry.dao.entity.patient_info.patientplan.PatientPlan;
+import com.rustam.modern_dentistry.dao.entity.patient_info.patientplan.PatientPlanMain;
 import com.rustam.modern_dentistry.dao.repository.patient_info.patientplan.PatientPlansRepository;
 import com.rustam.modern_dentistry.dto.request.create.PatientPlansCreateRequest;
+import com.rustam.modern_dentistry.dto.request.create.PatientTreatmentRequest;
 import com.rustam.modern_dentistry.dto.response.create.PatientPlansResponse;
-import com.rustam.modern_dentistry.dto.response.read.CategoryOfOperationDto;
-import com.rustam.modern_dentistry.dto.response.read.OperationOfCategoryDto;
-import com.rustam.modern_dentistry.dto.response.read.PatientPlanPartOfToothDetailDto;
-import com.rustam.modern_dentistry.dto.response.read.PatientPlansDto;
+import com.rustam.modern_dentistry.dto.response.read.*;
 import com.rustam.modern_dentistry.exception.custom.ExistsException;
 import com.rustam.modern_dentistry.exception.custom.NotFoundException;
 import jakarta.persistence.Column;
@@ -15,9 +14,13 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -49,16 +52,48 @@ public class PatientPlanUtilService {
                         ).toList()).build()).build();
     }
 
-    public PatientPlan findById(UUID id){
+    public PatientPlan findById(UUID id) {
         return patientPlansRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("No such patient plan found."));
     }
 
     public void validate(UUID patientPlanMainId, Long toothId, Long categoryId, List<Long> partOfTeethIds, Long operationId) {
         if (patientPlansRepository.existsByToothAndOperations(
-                patientPlanMainId,toothId,categoryId,partOfTeethIds,operationId
-        )){
+                patientPlanMainId, toothId, categoryId, partOfTeethIds, operationId
+        )) {
             throw new ExistsException("gonderilen datalar db-de movcuddur");
         }
+    }
+
+    public PatientPlanMain existsByDateOfPatientPlanMain(UUID planMainId) {
+        return patientPlansRepository.findByDateOfPatientPlanMain(planMainId)
+                .orElseThrow(() -> new NotFoundException("PatientPlanMain not found with given criteria"));
+    }
+
+    @Transactional
+    public List<ReadByPatientPlanMainIdOfTreatment> mapToTreatmentDto(PatientPlanMain planMain) {
+        if (planMain == null || planMain.getPatientPlans() == null) {
+            return Collections.emptyList();
+        }
+
+        return planMain.getPatientPlans().stream()
+                .map(plan -> ReadByPatientPlanMainIdOfTreatment.builder()
+                        .key(planMain.getKey())
+                        .patientPlanId(plan.getId())
+                        .toothNo(plan.getToothId())
+                        .details(plan.getDetails().stream()
+                                .map(detail -> PatientPlansOfTreatmentResponse.builder()
+                                        .id(detail.getId())
+                                        .partOfToothId(detail.getPartOfToothId())
+                                        .operationName(detail.getOpTypeItem().getOperationName())
+                                        .price(detail.getOpTypeItem().getAmount())
+                                        .build())
+                                .collect(Collectors.toList()))
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    public List<PatientPlan> findAllById(List<UUID> list) {
+        return patientPlansRepository.findAllById(list);
     }
 }
