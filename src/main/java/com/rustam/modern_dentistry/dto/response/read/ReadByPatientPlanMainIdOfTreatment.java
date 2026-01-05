9a7f7dc0e34d96442caca1fe62e12a3a5b97dfa4
rustamhuseynov-dev <rustem.huseynov.2016@gmail.com>
2026-01-05 13:01:34 +0400

    Create ReadByPatientPlanMainIdOfTreatment.java

---
+++ src/main/java/com/rustam/modern_dentistry/dto/response/read/ReadByPatientPlanMainIdOfTreatment.java
@@ -0,0 +1,20 @@
+package com.rustam.modern_dentistry.dto.response.read;
+
+import com.rustam.modern_dentistry.dto.response.create.PatientPlansResponse;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ReadByPatientPlanMainIdOfTreatment {
+    String key;
+    UUID patientPlanId;
+    Long toothNo;
+    List<PatientPlansOfTreatmentResponse> details;
+}
